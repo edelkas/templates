0cc7b6e4ea378c8e89810594ba17884fab4c7d3d
edelkas <edlucasma@gmail.com>
2020-10-10 03:06:02 +0200

    improving table

--- imgui/src/main.cpp
+++ imgui/src/main.cpp
@@ -21,11 +21,25 @@ static void glfw_error_callback(int error, const char* description)
   fprintf(stderr, "Glfw Error %d: %s\n", error, description);
 }
 
+static void HelpMarker(const char* desc)
+{
+    ImGui::TextDisabled("(?)");
+    if (ImGui::IsItemHovered())
+    {
+        ImGui::BeginTooltip();
+        ImGui::PushTextWrapPos(ImGui::GetFontSize() * 35.0f);
+        ImGui::TextUnformatted(desc);
+        ImGui::PopTextWrapPos();
+        ImGui::EndTooltip();
+    }
+}
+
 void create_window(int window_x, int window_y, int window_w, int window_h) {
   ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoTitleBar |
                                   ImGuiWindowFlags_NoMove |
                                   ImGuiWindowFlags_NoResize |
-                                  ImGuiWindowFlags_NoCollapse;
+                                  ImGuiWindowFlags_NoCollapse |
+                                  ImGuiWindowFlags_AlwaysUseWindowPadding;
   ImGui::SetNextWindowPos(ImVec2(window_x, window_y), ImGuiCond_FirstUseEver);
   ImGui::SetNextWindowSize(ImVec2(window_w, window_h), ImGuiCond_FirstUseEver);
   ImGui::Begin(" ", NULL, window_flags);
@@ -89,24 +103,7 @@ int main(int, char**)
   // Don't create ini config file
   io.IniFilename = NULL;
 
-  // Load Fonts
-  // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
-  // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
-  // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-  // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
-  // - Read 'docs/FONTS.md' for more instructions and details.
-  // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-  //io.Fonts->AddFontDefault();
-  //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
-  //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
-  //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
-  //io.Fonts->AddFontFromFileTTF("../../misc/fonts/ProggyTiny.ttf", 10.0f);
-  //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
-  //IM_ASSERT(font != NULL);
-
   // Our state
-  bool show_demo_window = true;
-  bool show_another_window = false;
   ImVec4 clear_color = ImVec4(0.0586f, 0.0586f, 0.0586f, 0.9375f);
 
   // Main loop
@@ -123,48 +120,113 @@ int main(int, char**)
     ImGui_ImplOpenGL3_NewFrame();
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
-    ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoTitleBar |
-                                      ImGuiWindowFlags_NoMove |
-                                      ImGuiWindowFlags_NoResize |
-                                      ImGuiWindowFlags_NoCollapse;
 
-    // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
     {
-      static bool tabs[6] = { true, true, true, true, true, true };
-      static bool states[3] = { true, true, true };
-      static bool types[3] = { true, false, false };
-      static bool modes[4] = { true, false, false, false };
-      const char* titles[4] = { "Tabs", "Types", "Modes", "States" };
-      const char* s_tabs[6] = { "SI", "S", "SU", "SL", "?", "!" };
-      const char* s_types[3] = { "Levels", "Episodes", "Stories" };
-      const char* s_modes[4] = { "Solo", "Coop", "Race", "Hardcore" };
+      /* Data */
+      static bool tabs[6]     = { true, true, true, true, true, true };
+      static bool states[3]   = { true, true, true };
+      static bool types[3]    = { true, false, false };
+      static bool modes[4]    = { true, false, false, false };
+      static bool orders[7]   = { true, false, false, false, false, false, false };
+      static bool rev_order   = false;
+      const char* titles[4]   = { "Tabs", "Types", "Modes", "States" };
+      const char* s_tabs[6]   = { "SI", "S", "SU", "SL", "?", "!" };
+      const char* s_types[3]  = { "Levels", "Episodes", "Stories" };
+      const char* s_modes[4]  = { "Solo", "Coop", "Race", "Hardcore" };
       const char* s_states[3] = { "Locked", "Unlocked", "Completed" };
-      create_window(0, 0, 600, 500);
+      const char* headers[7]  = { "ID", "State", "Atts.", "Vics.", "Gold", "Score", "Rank" };
+      int window_x            = 0;
+      int window_y            = 0;
+      int window_w            = 600;
+      int window_h            = 500;
+      int col_width           = 75;
+      int col_offset          = 10;
+
+      /* Header */
+      create_window(window_x, window_y, window_w, window_h);
+      ImGui::Text("SAVEFILE ANALYSIS"); ImGui::SameLine();
+      ImGui::SmallButton("Open savefile"); ImGui::SameLine();
+      ImGui::Text("Username");
+
+      /* Checkboxes */
       ImGui::Columns(4);
+      ImGui::Separator();
       for (int i = 0; i < 4; i++) {
         ImGui::Text("%s", titles[i]); ImGui::NextColumn();
       }
       ImGui::Separator();
+      ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(2, 0));
+      ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(2, 0));
       for (int i = 0; i < 6; i++) {
                    ImGui::Checkbox(s_tabs[i],   &tabs[i]);   ImGui::NextColumn();
         if (i < 3) ImGui::Checkbox(s_types[i],  &types[i]);  ImGui::NextColumn();
         if (i < 4) ImGui::Checkbox(s_modes[i],  &modes[i]);  ImGui::NextColumn();
         if (i < 3) ImGui::Checkbox(s_states[i], &states[i]); ImGui::NextColumn();
       }
+      ImGui::PopStyleVar(2);
+
+      /* Table headers */
       ImGui::Columns(7);
+      ImGui::SetColumnWidth(0, col_width + col_offset);
       ImGui::Separator();
-      const char* headers[7] = {"ID", "State", "Attempts", "Victories", "Gold", "Score", "Rank"};
       for (int i = 0; i < 7; i++) {
+        bool highlight = orders[i];
+        if (highlight) {
+          int c = rev_order ? 0 : 2;
+          ImGui::PushStyleColor(ImGuiCol_Button, (ImVec4)ImColor::HSV(c / 7.0f, 0.6f, 0.6f));
+          ImGui::PushStyleColor(ImGuiCol_ButtonHovered, (ImVec4)ImColor::HSV(c / 7.0f, 0.7f, 0.7f));
+          ImGui::PushStyleColor(ImGuiCol_ButtonActive, (ImVec4)ImColor::HSV(c / 7.0f, 0.8f, 0.8f));
+        }
+        if (ImGui::SmallButton("^")) {
+          if (highlight) {
+            rev_order = !rev_order;
+          } else {
+            for (int j = 0; j < 7; j++) orders[j] = false;
+            orders[i] = true;
+            rev_order = false;
+          }
+        }
+        if (highlight) ImGui::PopStyleColor(3);
+        ImGui::SameLine();
         ImGui::Text("%s", headers[i]);
         ImGui::NextColumn();
       }
+
+      /* Table contents */
+      ImGui::Columns(1);
       ImGui::Separator();
-      for (int i = 0; i < 20; i++) {
+      ImGui::BeginChild(" ", ImVec2(window_w - 20, window_h - 225));
+      ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0, 0));
+      ImGui::Columns(7);
+      ImGui::SetColumnWidth(0, col_width);
+      for (int i = 0; i < 2000; i++) {
         for (int j = 0; j < 7; j++) {
-          ImGui::Text("SI-A-00-00");
+          if (j == 0) {
+            ImGui::Text("SI-A-00-00");
+          } else {
+            ImGui::Text(" (%d, %d)", i, j);
+          }
           ImGui::NextColumn();
         }
       }
+      ImGui::PopStyleVar();
+      ImGui::EndChild();
+
+      /* Table footer */
+      ImGui::Columns(7);
+      ImGui::SetColumnWidth(0, col_width + col_offset);
+      ImGui::Separator();
+      ImGui::Text("Total"); ImGui::NextColumn(); ImGui::NextColumn();
+      ImGui::Text("Many"); ImGui::NextColumn();
+      ImGui::Text("Lots"); ImGui::NextColumn();
+      ImGui::Text("Abundant"); ImGui::NextColumn();
+      ImGui::Text("Seconds"); ImGui::NextColumn(); ImGui::NextColumn();
+      ImGui::Text("Avg."); ImGui::NextColumn(); ImGui::NextColumn();
+      ImGui::Text("Many"); ImGui::NextColumn();
+      ImGui::Text("Lots"); ImGui::NextColumn();
+      ImGui::Text("Abundant"); ImGui::NextColumn();
+      ImGui::Text("Seconds"); ImGui::NextColumn();
+      ImGui::Text("0th"); ImGui::NextColumn();
       ImGui::End();
     }
 
